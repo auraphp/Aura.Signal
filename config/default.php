c38d64608047b93c5c2ac76dbd89ca65ec3295c8
Paul M. Jones <pmjones88@gmail.com>
2011-02-27 10:19:59 -0600

    remove autoloader config

--- config/default.php
+++ config/default.php
@@ -1,10 +1,5 @@
 <?php
 /**
- * Autoloader information.
- */
-$loader->setPath('aura\signal\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
-
-/**
  * Dependency services.
  */
 $di->set('signal_manager', function() use ($di) {
@@ -14,16 +9,3 @@ $di->set('signal_manager', function() use ($di) {
         'result_collection' => new aura\signal\ResultCollection, // newInstance() fails with ArrayObject ?
     ));
 });
-
-/*
-// add signal handlers like so: when the vendor\package\Example class sends a 
-// 'pre_method' signal, invoke the provided closure.
-$di->config['aura\signal\Manager']['handlers'][] = array(
-    'vendor\package\Example',                       // sender
-    'signal_name',                                  // signal
-    function($foo, $bar) use ($di) {                // callback
-        $service = $di->get('some_service');
-        return $service->doSomething($foo, $bar);
-    },
-);
-*/
