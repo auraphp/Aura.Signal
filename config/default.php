387c0637da24b2488efa6bbe66bd0c27ec4b3606
Paul M. Jones <pmjones88@gmail.com>
2012-07-05 22:06:04 -0500

    set params separately from the service

--- config/default.php
+++ config/default.php
@@ -4,13 +4,15 @@
  */
 $loader->add('Aura\Signal\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
 
+$di->params['Aura\Signal\Manager'] = [
+    'handler_factory'   => $di->newInstance('Aura\Signal\HandlerFactory'),
+    'result_factory'    => $di->newInstance('Aura\Signal\ResultFactory'),
+    'result_collection' => $di->newInstance('Aura\Signal\ResultCollection'),
+];
+
 /**
  * Dependency services.
  */
 $di->set('signal_manager', function() use ($di) {
-    return $di->newInstance('Aura\Signal\Manager', [
-        'handler_factory'   => $di->newInstance('Aura\Signal\HandlerFactory'),
-        'result_factory'    => $di->newInstance('Aura\Signal\ResultFactory'),
-        'result_collection' => new Aura\Signal\ResultCollection, // newInstance() fails with ArrayObject ?
-    ]);
+    return $di->newInstance('Aura\Signal\Manager');
 });
