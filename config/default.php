af6b405f3bac60020d5ab583ec18718d8604e0fe
Paul M. Jones <pmjones88@gmail.com>
2012-11-08 09:02:52 -0700

    signal manager will be set up in Aura.Framework, not here

--- config/default.php
+++ config/default.php
@@ -10,8 +10,3 @@ $loader->add('Aura\Signal\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
 $di->params['Aura\Signal\Manager']['handler_factory']   = $di->newInstance('Aura\Signal\HandlerFactory');
 $di->params['Aura\Signal\Manager']['result_factory']    = $di->newInstance('Aura\Signal\ResultFactory');
 $di->params['Aura\Signal\Manager']['result_collection'] = $di->newInstance('Aura\Signal\ResultCollection');
-
-/**
- * Dependency services.
- */
-$di->set('signal_manager', $di->lazyNew('Aura\Signal\Manager'));
